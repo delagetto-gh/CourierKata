5df462c4fa383c23527b97c4ad34864f74737154
delagetto-gh <delagetto-gh@users.noreply.github.com>
2019-02-12 19:12:24 +0000

    created first set of unit tests for ensuring API does as expected

---
+++ Tests/CourierKata.Api.Tests/DeliveryCostCalculatorApiTests.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using Moq;
+using Xunit;
+
+namespace CourierKata.Api.Tests
+{
+    /*
+    "Output should be a collection of items with their individual cost and type, as well as
+    total cost
+     */
+    public class DeliveryCostCalculatorApiTests
+    {
+        private readonly IDeliveryCostCalculatorApi sut;
+
+        public DeliveryCostCalculatorApiTests()
+        {
+
+            this.sut = new DeliveryCostCalculatorApi();
+        }
+
+        [Fact]
+        public void ShouldReturnParcelCosts()
+        {
+            var order = new OrderDto
+            {
+                Parcels = new List<ParcelDto>
+     {
+                     new ParcelDto{Height = 4, Width = 4},
+                     new ParcelDto{Height = 20, Width = 25},
+                     new ParcelDto{Height = 20, Width = 25},
+        }
+            };
+
+            var result = this.sut.CalculateCost(order);
+
+            Assert.Equal(3, result.Items[0].Cost);
+            Assert.Equal(8, result.Items[1].Cost);
+            Assert.Equal(8, result.Items[2].Cost);
+        }
+
+        [Fact]
+        public void ShouldReturnParcelTypes()
+        {
+            var order = new OrderDto
+            {
+                Parcels = new List<ParcelDto>
+     {
+                     new ParcelDto{Height = 4, Width = 4},
+                     new ParcelDto{Height = 20, Width = 25},
+                     new ParcelDto{Height = 20, Width = 25},
+        }
+            };
+
+            var result = this.sut.CalculateCost(order);
+
+            Assert.Equal("SmallParcel", result.Items[0].Type);
+            Assert.Equal("MediumParcel", result.Items[1].Type);
+            Assert.Equal("MediumParcel", result.Items[2].Type);
+        }
+
+        [Fact]
+        public void ShouldReturnTotalCost()
+        {
+            var order = new OrderDto
+            {
+                Parcels = new List<ParcelDto>
+     {
+                     new ParcelDto{Height = 4, Width = 4},
+                     new ParcelDto{Height = 20, Width = 25},
+                     new ParcelDto{Height = 20, Width = 25},
+        }
+            };
+
+            var result = this.sut.CalculateCost(order);
+
+            Assert.Equal(19, result.TotalCost);
+        }
+    }
+}
+
