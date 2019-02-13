ba42bf801cf88627c453eb7f1d530320d1cd502a
delagetto-gh <delagetto-gh@users.noreply.github.com>
2019-02-13 00:22:56 +0000

    created a console project to test runtime & correct output

---
+++ CourierKata.App/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using CourierKata.Api;
+using CourierKata.Domain;
+
+namespace CourierKata.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parcelDelivCalcService = new ParcelDeliveryCostCalculatorService();
+            var api = new DeliveryCostCalculatorApi(parcelDelivCalcService);
+
+            var order = new OrderDto
+            {
+                Parcels = new List<ParcelDto>
+                {
+                    new ParcelDto{Height = 4, Width = 4},
+                    new ParcelDto{Height = 20, Width = 25},
+                    new ParcelDto{Height = 20, Width = 25}
+                }
+            };
+
+            var output = api.CalculateCost(order);
+
+            foreach (var item in output.Items)
+            {
+                Console.WriteLine($"Item:{item.Type}, Cost:${item.Cost}");
+            }
+
+            Console.WriteLine($"Total:${output.TotalCost}");
+        }
+    }
+}
